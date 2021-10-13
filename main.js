772d34937df5eb81032e77caa07700a1772a363c
Bowei Xu <boweixu929@gmail.com>
2021-10-13 17:33:03 -0400

    mvp

---
+++ main.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+//process of registring slash command
+//put bot online 
+const {Client, Intents} = require('discord.js');
+const client = new Client({intents: [Intents.FLAGS.GUILDS]});
+client.once('ready', ()=>{
+  console.log("I am ready")
+});
+
+client.login(`${process.env.BOT_TOKEN}`);
+
+client.on('interactionCreate', async interaction =>{
+  if (!interaction.isCommand()) return;
+	const { commandName } = interaction;
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply('This is a testfield for bot');
+	} else if (commandName === 'user') {
+		await interaction.reply('User info.');
+	}
+})
